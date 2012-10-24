b24afb0a9cb14d574bd33da125dfb6cae0ab9e61
azlen <azlen@livingcode.org>
2012-10-23 21:25:28 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,121 @@
+var mapCanvas = document.getElementById("map");
+var canvas = document.getElementById("canvas");
+var map = mapCanvas.getContext("2d");
+var ctx = canvas.getContext("2d");
+
+var mouseX = 0;
+var mouseY = 0;
+
+var player = {
+	x: 500,
+	y: 500,
+	w: 15,
+	h: 15,
+	move: false,
+	hit: false,
+	line_angle: 0,
+	action: "shoot",
+	bullet: {
+		x: 0,
+		y: 0,
+		angle: 0,
+		created: false
+	}
+}
+
+document.onmousemove = function(event){
+	mouseX = event.clientX;
+	mouseY = event.clientY;
+};
+
+document.onmousedown = function(event){
+	if(player.action === "move"){
+		if(player.move === false){
+			player.move = true;
+			player.line_angle = Math.atan2(mouseY - (player.y + (player.h/2)), mouseX - (player.x + (player.w/2)));
+		}
+	}else if(player.action === "shoot"){
+		player.bullet.created = true;
+		player.bullet.x = player.x + (player.w/2);
+		player.bullet.y = player.y + (player.h/2);
+		player.bullet.angle = Math.atan2(mouseY - (player.y + (player.h/2)), mouseX - (player.x + (player.w/2)));
+	}
+};
+
+document.onkeydown = function(event){
+	keycode = event.keyCode;
+	if(keycode === 49){
+		player.action = "shoot";
+	}
+	if(keycode === 50){
+		player.action = "move";
+	}
+}
+
+function clear(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function draw(){
+	clear();
+	
+	if(player.move === true){
+		var pixel = map.getImageData(player.x + (player.w/2) + (Math.cos(player.line_angle)*5), player.y + (player.h/2) + (Math.sin(player.line_angle)*5), 1, 1);
+		if(pixel.data[3] === 0){
+			player.x += Math.cos(player.line_angle)*5;
+			player.y += Math.sin(player.line_angle)*5;
+		}else{
+			if(player.hit === false){
+				player.hit = true;
+				player.line_angle = Math.PI*0.5;
+			}else{
+				player.hit = false;
+				player.move = false;
+			}
+		}
+	}
+	//CREATE THE AIMING LINE
+	ctx.beginPath();
+	ctx.moveTo((player.x+(player.w/2)), (player.y+(player.h/2)));
+	ctx.lineTo(Math.cos(Math.atan2(mouseY - (player.y + (player.h/2)), mouseX - (player.x + (player.w/2))))*100000 + player.x + (player.w/2), Math.sin(Math.atan2(mouseY - (player.y + (player.h/2)), mouseX - (player.x + (player.w/2))))*100000 + player.y + (player.h/2));
+	ctx.closePath();
+	ctx.stroke();
+	
+	
+	//CREATE THE PLAYER (circle)
+	ctx.beginPath();
+	ctx.arc((player.x+(player.w/2)), (player.y+(player.h/2)), player.w, 0, Math.PI*2, true); 
+	ctx.closePath();
+	ctx.fill();
+	
+	if(player.bullet.created === true){
+		var pixel = map.getImageData(player.bullet.x + (Math.cos(player.bullet.angle)*5), player.bullet.y + (Math.sin(player.bullet.angle)*5), 1, 1);
+		if(pixel.data[3] === 0){
+			player.bullet.x += Math.cos(player.bullet.angle)*5;
+			player.bullet.y += Math.sin(player.bullet.angle)*5;
+		}else{
+			map.fillStyle = "#fff";
+			map.beginPath();
+			map.arc(player.bullet.x, player.bullet.y, 30, 0, Math.PI*2, true); 
+			map.closePath();
+			map.fill();
+			player.line_angle = Math.PI*0.5;
+			player.move = true;
+			player.bullet.created = false;
+		}
+		ctx.beginPath();
+		ctx.arc(player.bullet.x, player.bullet.y, 5, 0, Math.PI*2, true); 
+		ctx.closePath();
+		ctx.fill();
+	}
+	
+	ctx.fillText(player.action, 10, 10);
+}
+
+setInterval(draw, 1000/60);
+
+map.fillRect(0, 170, 1000, 40);
+map.fillRect(0, 430, 1000, 40);
+map.fillRect(0, 660, 1000, 40);
+
+map.globalCompositeOperation = "destination-out";
